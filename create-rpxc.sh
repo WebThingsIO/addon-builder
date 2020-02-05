45242aa634101abf80817e36d54dc6f6bdff98a9
Michael Stegeman <michael@stegeman.me>
2020-02-05 08:10:53 -0900

    Update toolchain.

--- create-rpxc.sh
+++ create-rpxc.sh
@@ -8,6 +8,5 @@ mkdir -p $(dirname ${RPXC})
 
 # Build the docker raspberry pi cross compiler
 echo "Creating rpxc"
-docker run -t dhylands/raspberry-pi-cross-compiler-stretch | tr -d $'\r' > ${RPXC}
-sed -i 's/docker run -i -t/docker run -t/' ${RPXC}
+docker run -t mozillaiot/raspberry-pi-cross-compiler-stretch | tr -d $'\r' > ${RPXC}
 chmod +x ${RPXC}
